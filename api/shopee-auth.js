971ed384a9cabcac77a3eec7bcf57d7541ab5b63
wildanmirtaza <wildanmirtaza@gmail.com>
2026-07-18 17:25:18 +0700

    Verify Partner Key

--- api/shopee-auth.js
+++ api/shopee-auth.js
@@ -4,7 +4,11 @@ export default function handler(req, res) {
 
         success: true,
 
-        partnerId: process.env.SHOPEE_PARTNER_ID
+        partnerId: process.env.SHOPEE_PARTNER_ID,
+
+        partnerKey: process.env.SHOPEE_PARTNER_KEY
+            ? "FOUND"
+            : "NOT FOUND"
 
     });
 
