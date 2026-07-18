3bb02d43e0e138f31d144d8236963219f7a3f184
wildanmirtaza <wildanmirtaza@gmail.com>
2026-07-18 16:58:00 +0700

    Read Shopee env variables

--- api/hello.js
+++ api/hello.js
@@ -3,7 +3,10 @@ export default function handler(req, res) {
     res.status(200).json({
 
         success: true,
-        message: "Hello from Vercel Function"
+
+        partnerId: process.env.SHOPEE_PARTNER_ID ? "OK" : "NOT FOUND",
+
+        partnerKey: process.env.SHOPEE_PARTNER_KEY ? "OK" : "NOT FOUND"
 
     });
 
