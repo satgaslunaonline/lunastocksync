11094423a7cc341b44919802cb3ed256bfc8be12
wildanmirtaza <wildanmirtaza@gmail.com>
2026-07-19 16:56:36 +0700

    products

---
+++ api/products.js
@@ -0,0 +1,40 @@
+import crypto from "crypto";
+
+export default async function handler(req, res) {
+
+    const partnerId = process.env.SHOPEE_PARTNER_ID;
+    const partnerKey = process.env.SHOPEE_PARTNER_KEY;
+
+    // sementara hardcode dulu
+    const accessToken = "784a4c6d7647635672594b5575614269";
+    const shopId = 304158815;
+
+    const path = "/api/v2/product/get_item_list";
+
+    const timestamp = Math.floor(Date.now() / 1000);
+
+    const baseString =
+        `${partnerId}${path}${timestamp}${accessToken}${shopId}`;
+
+    const sign = crypto
+        .createHmac("sha256", partnerKey)
+        .update(baseString)
+        .digest("hex");
+
+    const url =
+        `https://partner.shopeemobile.com${path}` +
+        `?partner_id=${partnerId}` +
+        `&timestamp=${timestamp}` +
+        `&access_token=${accessToken}` +
+        `&shop_id=${shopId}` +
+        `&sign=${sign}` +
+        `&offset=0` +
+        `&page_size=20`;
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+
+}
