2e855299446e1f9397a218e56962369a6b09f89d
wildanmirtaza <wildanmirtaza@gmail.com>
2026-07-19 12:14:26 +0700

    shopee-auth

--- api/shopee-auth.js
+++ api/shopee-auth.js
@@ -2,36 +2,25 @@ import crypto from "crypto";
 
 export default function handler(req, res) {
 
-    const partnerId = Number(process.env.SHOPEE_PARTNER_ID);
-    const partnerKey = process.env.SHOPEE_PARTNER_KEY.replace(/^shpk/, "");
-
-    const redirect = "https://lunastocksync.vercel.app/auth/callback";
+    const partnerId = process.env.SHOPEE_PARTNER_ID;
+    const partnerKey = process.env.SHOPEE_PARTNER_KEY;
 
     const timestamp = Math.floor(Date.now() / 1000);
+    const path = "/api/v2/shop/auth_partner";
 
-const path = "/api/v2/shop/auth_partner";
-
-const baseString = `${partnerId}${path}${timestamp}`;
+    const baseString = `${partnerId}${path}${timestamp}`;
 
     const sign = crypto
         .createHmac("sha256", partnerKey)
         .update(baseString)
         .digest("hex");
 
-    const url =
-        `https://partner.shopeemobile.com/api/v2/shop/auth_partner` +
-        `?partner_id=${partnerId}` +
-        `&timestamp=${timestamp}` +
-        `&sign=${sign}` +
-        `&redirect=${encodeURIComponent(redirect)}`;
-
-res.status(200).json({
-    partnerId,
-    timestamp,
-    path,
-    baseString,
-    sign,
-    url
-});
+    res.json({
+        partnerId,
+        keyPrefix: partnerKey.substring(0, 8),
+        keyLength: partnerKey.length,
+        baseString,
+        sign
+    });
 
 }
