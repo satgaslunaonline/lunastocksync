ba66496bb979c0fa7b2b53f71cf6898d00b7bd76
wildanmirtaza <wildanmirtaza@gmail.com>
2026-07-19 17:47:42 +0700

    tabel shopee products

--- src/pages/shopee-products.js
+++ src/pages/shopee-products.js
@@ -3,13 +3,17 @@ import { getProducts } from "../services/shopee.js";
 export function renderShopeeProducts() {
 
     return `
+
         <h2>Shopee Products</h2>
 
         <button id="btnRefresh">
+
             Refresh dari Shopee
+
         </button>
 
         <div id="result"></div>
+
     `;
 
 }
@@ -30,13 +34,66 @@ export function initShopeeProducts() {
                 const data =
                     await getProducts();
 
-                result.innerHTML =
-                    `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+                const items =
+                    data.response.item;
 
-            } catch (err) {
+                let html = `
 
-                result.innerHTML =
-                    err.message;
+<table border="1" cellspacing="0" cellpadding="8">
+
+<tr>
+
+<th>Item ID</th>
+
+<th>Status</th>
+
+<th>Update</th>
+
+<th>Aksi</th>
+
+</tr>
+
+`;
+
+                items.forEach(item => {
+
+                    html += `
+
+<tr>
+
+<td>${item.item_id}</td>
+
+<td>${item.item_status}</td>
+
+<td>${new Date(item.update_time * 1000).toLocaleString()}</td>
+
+<td>
+
+<button
+class="btn-detail"
+data-id="${item.item_id}">
+
+Detail
+
+</button>
+
+</td>
+
+</tr>
+
+`;
+
+                });
+
+                html += "</table>";
+
+                result.innerHTML = html;
+
+            }
+
+            catch (err) {
+
+                result.innerHTML = err.message;
 
             }
 
