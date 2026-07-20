4d02fc97cf2089deb45c7ba72734cd37141418eb
wildanmirtaza <wildanmirtaza@gmail.com>
2026-07-20 15:23:26 +0700

    sync products ubah handler 2

--- api/sync-products.js
+++ api/sync-products.js
@@ -1,15 +1,31 @@
 import { db } from "./lib/firebase-admin.js";
 import { getAllProducts } from "./lib/products.js";
+import { FieldValue } from "firebase-admin/firestore";
 
 export default async function handler(req, res) {
 
     const products = await getAllProducts();
 
+    if (products.length > 0) {
+
+        await db
+            .collection("products")
+            .doc(String(products[0].item_id))
+            .set({
+
+                ...products[0],
+
+                updatedAt: FieldValue.serverTimestamp()
+
+            });
+
+    }
+
     res.status(200).json({
 
-        total: products.length,
+        success: true,
 
-        products
+        total: products.length
 
     });
 
